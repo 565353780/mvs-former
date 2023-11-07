79a3eb3c65666e10ca31ca49b731414511dd97d7
chLi <565353780@qq.com>
2023-11-07 19:18:28 +0800

    add fusibile lib support

--- dev_setup.sh
+++ dev_setup.sh
@@ -1,5 +1,12 @@
 cd ..
 git clone https://github.com/ewrfcas/MVSFormer.git
+git clone https://github.com/YoYo000/fusibile.git
+
+cd mvs-fusion/mvs_fusion/Lib/fusibile
+mkdir build
+cd build
+cmake ..
+make -j
 
 pip install numpy omegaconf opencv_python Pillow plyfile \
 	PyYAML tensorboardX timm tqdm
